3a0b13d08e37cd4c232dcc8c0ac866c07dc94ed8
Manuel Rodriguez <cuentapersonal.0098@gmail.com>
2024-07-07 20:54:43 -0500

    Actualizacion el evento click ya funciona solo al hacer click en el nombre del producto

---
+++ src/uses-cases/add-x-o-check.js
@@ -0,0 +1,38 @@
+
+
+
+export const addX = ( selectDivProducts )=>{ 
+
+    const selectAncestroText = selectDivProducts.closest('div');
+    const selectAncestroIcon = selectAncestroText.querySelector('i');
+
+
+    //Le borrar las clase de check y le pone la de la X
+    selectAncestroIcon.classList.remove('span-product__icon--green');
+    selectAncestroIcon.classList.remove('fa-solid');
+    selectAncestroIcon.classList.remove('fa-check');
+
+    // Le pone una clase que tiene un tachado y le agrega una X
+    selectAncestroIcon.classList.add('span-product__icon--red');
+    selectAncestroIcon.classList.add('fa-solid');
+    selectAncestroIcon.classList.add('fa-xmark');
+
+
+
+};
+
+export const addCheck = ( selectDivProducts )=>{ 
+
+    const selectAncestroText = selectDivProducts.closest('div');
+    const selectAncestroIcon = selectAncestroText.querySelector('i');
+
+    selectAncestroIcon.classList.remove('span-product__icon--red');
+    selectAncestroIcon.classList.remove('fa-solid'); 
+    selectAncestroIcon.classList.remove('fa-xmark');
+
+    selectAncestroIcon.classList.add('span-product__icon--green');
+    selectAncestroIcon.classList.add('fa-solid');
+    selectAncestroIcon.classList.add('fa-check');
+
+
+ };
--- src/uses-cases/event-click.js
+++ src/uses-cases/event-click.js
@@ -1,3 +1,4 @@
+import { addCheck, addX } from "./add-x-o-check";
 import { LoadStaticTotalPrice, containerInfo, infoResultTotal, inputsSaves, priceSaves, sumaTotalPrice } from "./create-product";
 import { loadProductDom, showTotalPrice } from "./create-product-dom";
 import { emptyArrayProductUndefined } from "./empty-array-element-undefined";
@@ -5,7 +6,8 @@ import { ids } from "./generator-ids";
 import { loadTotalPriceLineThrough } from "./load-total-price-line-through";
 import { indexsFound } from "./save-class-line-through";
 
-export let clickIndexFound = []; 
+export let clickIndexFound = [];
+const selectProductDOM = document.querySelector('.info__product');
 
 export const loadClickIndexFound = ()=>{ 
 
@@ -19,115 +21,115 @@ export const loadClickIndexFound = ()=>{
 
 export const eventClick = (element)=>{ 
 
-    containerInfo.addEventListener('click', (event)=>{
-
-        const idsProduct = event.target.id;
-
-        const indexElementSelect = ids.indexOf(idsProduct);
-
-        // console.log(ids);
-        
-        // console.log(indexElementSelect);
-
-        // Selecciona un elemento del DOM por el ID
-        const selectDivProducts = document.getElementById(`${event.target.id}`);
-        
-
-        // El "closest('Selector')" sirve para seleccionar el ancestro mas cercano del elemento html que se le pase
-        const selectAncestroText = selectDivProducts.closest('div');
-        const selectAncestroIcon = selectAncestroText.querySelector('i');
-
-        if(idsProduct){
-
-            // Consigue los precios del localStorage
-            let getPriceSavesLocal = JSON.parse(localStorage.getItem( 4 ) );
-
-            if( selectAncestroText.className === 'info__span-product' ){
-
-                //Le borrar las clase de check y le pone la de la X
-                // selectAncestroIcon.classList.remove('span-product__icon--green');
-                // selectAncestroIcon.classList.remove('fa-solid');
-                // selectAncestroIcon.classList.remove('fa-check');
-
-                // Le pone una clase que tiene un tachado y le agrega una X
-                // selectAncestroIcon.classList.add('span-product__icon--red');
-                // selectAncestroIcon.classList.add('fa-solid');
-                // selectAncestroIcon.classList.add('fa-xmark');
-                selectAncestroText.classList.add('line-through');
-
-                //Guardo el indice del producto tachado por su ID
-                clickIndexFound.push(indexElementSelect);
-
-                // console.log(clickIndexFound);
-
-
-
-                // splice seleccona un elemento por su indice, se seleciona el numero apartir de cuantos se quieren cambiar y se le pasa el valor a cambiar
-                getPriceSavesLocal.splice(indexElementSelect, 1 , inputsSaves[indexElementSelect][1][1] );
-                //Cambia el valor del precio guardado en la ejecucion para que no sea tomado en cuenta si un producto esta tachado
-                priceSaves.splice(indexElementSelect, 1 , inputsSaves[indexElementSelect][1][1] );
-
-                // console.log(getPriceSavesLocal);
-
-                //Guarda el inputSaves con el price cambiado 
-                let setPriceChange = localStorage.setItem( 4, JSON.stringify( getPriceSavesLocal ) );
-
-                showTotalPrice();
-
-                
-            } else if( selectAncestroText.className === 'info__span-product line-through' ){
     
-                const saveTotalPrice  = inputsSaves[indexElementSelect][1][0];
-                const saveTotalAmount = inputsSaves[indexElementSelect][2];
+            console.log('click');
+        containerInfo.addEventListener('click', (event)=>{
+            // selectProductDOM.addEventListener('click', (event)=>{
 
-///////////////////////////////////////////////////////
+        // Solo permite que el evento click funcione al precionar el nombre del producto        
+           if( event.target.id ){
 
-// Ya se encontro como sustituir cada indice que no tiene tachado ahora falta mantenerlo guardado
-                // Encuentra el indice del elemento que ya no esta tachado
-                const findIndex = clickIndexFound.indexOf(indexElementSelect);
+            const idsProduct = event.target.id;
 
-                // Agarra el indice que ya no esta tachado y lo pone a undefined
-                clickIndexFound.splice(findIndex, 1, 'undefined');
+            const indexElementSelect = ids.indexOf(idsProduct);
 
-                // Filtra a los indices que su valor fue cambiado a undefined y sacar un nuevo array sin ellos
-                clickIndexFound = clickIndexFound.filter( ( data ) => {
+            // console.log(ids);
+            
+            // console.log(indexElementSelect);
 
-                    return data != 'undefined';
-                });
-                
-                //Todo Aca muetra el array de los indice que pertenecen a cada elemeto que estan tachado
-                // console.log(clickIndexFound);
+            // Selecciona un elemento del DOM por el ID
+            const selectDivProducts = document.getElementById(`${event.target.id}`);
+            
 
-                //Quita todos los valores undefined del array clickIndexFound
-                // emptyArrayElementUndefined();
+            // El "closest('Selector')" sirve para seleccionar el ancestro mas cercano del elemento html que se le pase
+            const selectAncestroText = selectDivProducts.closest('div');
+            
 
-///////////////////////////////////
+            if( idsProduct ){
 
-                 // splice seleccona un elemento por su indice, se seleciona el numero apartir de cuantos se quieren cambiar y se le pasa el valor a cambiar
-                 const savePriceChangeLocal = getPriceSavesLocal.splice(indexElementSelect, 1 , parseInt( saveTotalPrice * saveTotalAmount ) );
+                // Consigue los precios del localStorage
+                let getPriceSavesLocal = JSON.parse(localStorage.getItem( 4 ) );
 
-                const saveTotalPriceLocal  = localStorage.setItem( 4, JSON.stringify( getPriceSavesLocal ) );
-                
-                //Le borrar las clase de X y le pone la de la check
-                // selectAncestroIcon.classList.remove('span-product__icon--red');
-                // selectAncestroIcon.classList.remove('fa-solid'); 
-                // selectAncestroIcon.classList.remove('fa-xmark');
+                if( selectAncestroText.className === 'info__span-product' ){
 
-                // Le remueve una clase que tiene un tachado
-                selectAncestroText.classList.remove('line-through');
+                    //Agrega una X al tacharlo y le paso el div seleccionado como parametro
+                    addX( selectDivProducts );
 
-                // selectAncestroIcon.classList.add('span-product__icon--green');
-                // selectAncestroIcon.classList.add('fa-solid');
-                // selectAncestroIcon.classList.add('fa-check');
+                    selectAncestroText.classList.add('line-through');
 
-                 loadTotalPriceLineThrough();
+                    //Guardo el indice del producto tachado por su ID
+                    clickIndexFound.push(indexElementSelect);
 
-                // selectDivProducts.innerText = savePriceChangeLocal;
+                    // console.log(clickIndexFound);
 
-                showTotalPrice();
-                            
-            };        
-        };
 
-    });    
+
+                    // splice seleccona un elemento por su indice, se seleciona el numero apartir de cuantos se quieren cambiar y se le pasa el valor a cambiar
+                    getPriceSavesLocal.splice(indexElementSelect, 1 , inputsSaves[indexElementSelect][1][1] );
+                    //Cambia el valor del precio guardado en la ejecucion para que no sea tomado en cuenta si un producto esta tachado
+                    priceSaves.splice(indexElementSelect, 1 , inputsSaves[indexElementSelect][1][1] );
+
+                    // console.log(getPriceSavesLocal);
+
+                    //Guarda el inputSaves con el precio cambiado 
+                    let setPriceChange = localStorage.setItem( 4, JSON.stringify( getPriceSavesLocal ) );
+
+                    showTotalPrice();
+
+                    
+                } else if( selectAncestroText.className === 'info__span-product line-through' ){
+        
+                    const saveTotalPrice  = inputsSaves[indexElementSelect][1][0];
+                    const saveTotalAmount = inputsSaves[indexElementSelect][2];
+
+    ///////////////////////////////////////////////////////
+
+    // Ya se encontro como sustituir cada indice que no tiene tachado ahora falta mantenerlo guardado
+                    // Encuentra el indice del elemento que ya no esta tachado
+                    const findIndex = clickIndexFound.indexOf(indexElementSelect);
+
+                    // Agarra el indice que ya no esta tachado y lo pone a undefined
+                    clickIndexFound.splice(findIndex, 1, 'undefined');
+
+                    // Filtra a los indices que su valor fue cambiado a undefined y sacar un nuevo array sin ellos
+                    clickIndexFound = clickIndexFound.filter( ( data ) => {
+
+                        return data != 'undefined';
+                    });
+                    
+                    //Todo Aca muetra el array de los indice que pertenecen a cada elemeto que estan tachado
+                    // console.log(clickIndexFound);
+
+                    //Quita todos los valores undefined del array clickIndexFound
+                    // emptyArrayElementUndefined();
+
+    ///////////////////////////////////
+
+                    // splice seleccona un elemento por su indice, se seleciona el numero apartir de cuantos se quieren cambiar y se le pasa el valor a cambiar
+                    const savePriceChangeLocal = getPriceSavesLocal.splice(indexElementSelect, 1 , parseInt( saveTotalPrice * saveTotalAmount ) );
+
+                    const saveTotalPriceLocal  = localStorage.setItem( 4, JSON.stringify( getPriceSavesLocal ) );
+                    
+                    //Le borrar las clase de X, le pone la de la check y le paso el div seleccionado como parametro
+                    addCheck( selectDivProducts );
+
+                    // Le remueve una clase que tiene un tachado
+                    selectAncestroText.classList.remove('line-through');
+
+                   
+
+                    loadTotalPriceLineThrough();
+
+                    // selectDivProducts.innerText = savePriceChangeLocal;
+
+                    showTotalPrice();
+                                
+                };        
+            };
+
+
+
+           }
+
+        });       
 };
